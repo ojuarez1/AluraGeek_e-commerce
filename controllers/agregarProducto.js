bcd86d15806a0e34f2df9479da17be0946b217c1
ojuarez1 <oscarjuarez923@gmail.com>
2023-07-29 15:05:21 -0600

    Agregando archivo db json

--- controllers/agregarProducto.js
+++ controllers/agregarProducto.js
@@ -2,6 +2,7 @@ const contenido = document.querySelector("[data-container]");
 const dragText = contenido.querySelector("[data-text]");
 const button = document.querySelector("[data-boton]");
 const input = contenido.querySelector("#input-file");
+//const imagen = document.querySelector("[data-img]")
 let files;
 
 button.addEventListener("click", (event) => {
@@ -9,10 +10,10 @@ button.addEventListener("click", (event) => {
 })
 
 input.addEventListener("change", (e) => {
-    files = this.files;
+    files = e.target.files;
     contenido.classList.add("active");
     showFiles(files)
-    contenido.classList.remove("active");
+    //contenido.classList.remove("active");
 })
 
 contenido.addEventListener("dragover", (e) => {
@@ -31,7 +32,7 @@ contenido.addEventListener("drop", (e) => {
     e.preventDefault();
     files = e.dataTransfer.files;
     showFiles(files)
-    contenido.classList.remove("active");
+    //contenido.classList.remove("active");
     dragText.textContent ="Arrastra y suelta imagenes"
 })
 
@@ -51,7 +52,36 @@ const processFile = (file) => {
 
     if (validExtensions.includes(docType)) {
         //archivo valido
+        //imagen.src= `../assets/img/${file.name}`
+        //agregarProducto(file)
+        renderImage(file)
     } else {
         alert("Archivo no valido")
     }
+}
+
+const agregarProducto = (file) => {
+    const nombre = file.name
+    return fetch("http://localhost:3000/productos", {
+        method:"POST",
+        headers: {
+            "content-type":"application/json"
+        },
+        body: JSON.stringify({
+            nombre,
+        })
+    })
+}
+
+const imag = document.querySelector("[data-image]")
+const renderImage = (formData) => {
+    const image = URL.createObjectURL(formData);
+    console.log(image)
+    const imagen = document.createElement("img")
+    imagen.classList.add("dataImage")
+    imagen.setAttribute("src", image);
+    contenido.appendChild(imagen);
+    imag.classList.add("hidden")
+    dragText.classList.add("hidden");
+    
 }
