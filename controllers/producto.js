ff299d2ac416cce6150159a1ab46e13cdf3906ae
ojuarez1 <oscarjuarez923@gmail.com>
2023-07-30 16:53:13 -0600

    Creando archivo producto js

---
+++ controllers/producto.js
@@ -0,0 +1,10 @@
+const crearProducto = () => {
+    const contenido = `<li>
+        <img src="../assets/img/aaa.svg">
+        <div>
+            <p class="galeria__articulos-flex--precio">Producto XYZ</p>
+            <p class="galeria__articulos-flex--precio">$60</p>
+            <p class="galeria__articulos-flex--precio">#1111111</p>
+        </div>                        
+    </li>`
+}
