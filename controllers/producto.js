f11cd5b875802f7b0661d5c10fa39bc8ca09e8fe
ojuarez1 <oscarjuarez923@gmail.com>
2023-07-30 18:13:50 -0600

    Llamando datos del servidor

--- controllers/producto.js
+++ controllers/producto.js
@@ -1,10 +1,23 @@
-const crearProducto = () => {
+const crearProducto = (name, imageUrl, precio, descripcion) => {
+    const lista = document.querySelector("[data-lista]");
     const contenido = `<li>
-        <img src="../assets/img/aaa.svg">
+        <img src="${imageUrl}">
         <div>
-            <p class="galeria__articulos-flex--precio">Producto XYZ</p>
-            <p class="galeria__articulos-flex--precio">$60</p>
-            <p class="galeria__articulos-flex--precio">#1111111</p>
+            <p class="galeria__articulos-flex--precio">${name}</p>
+            <p class="galeria__articulos-flex--precio">${precio}</p>
+            <p class="galeria__articulos-flex--precio">${descripcion}</p>
         </div>                        
     </li>`
+    lista.innerHTML = contenido;
+    return lista
 }
+
+const showProduct = () => fetch("http://localhost:3000/productos").then((respuesta) => respuesta.json());
+showProduct().then((data) => {
+    data.forEach(perfil => {
+        const image = perfil.imageUrl;
+        console.log(crearProducto(perfil.name, image, perfil.precio, perfil.descripcion));
+
+    });
+})
+
