c29f4bec60f03f6bfdbf6bfeedc53629bedfb4bf
ojuarez1 <oscarjuarez923@gmail.com>
2023-07-31 23:08:09 -0600

    Programando barra de busqueda

--- controllers/barraBusqueda.js
+++ controllers/barraBusqueda.js
@@ -1,11 +1,19 @@
-const buscar = document.querySelector("[data-input-buscar]");
-
-
-buscar.addEventListener("keyup", (e) => {
+document.addEventListener("keyup", e => {
     const listaUl = document.querySelector("[data-lista]");
-    const listaLi = listaUl.querySelectorAll("li");
-    listaLi.forEach(producto => {
-        producto.textContent.toLowerCase().includes(e.target.value.toLowerCase());
+    const buscar = document.querySelectorAll(".buscado");
+    //console.log(e.target.matches("#buscador"))
+
+    buscar.forEach(producto => {
+        
+        if (producto.textContent.toLowerCase().includes(e.target.value.toLowerCase()) ) {
+            const filtrado = producto.parentElement
+            filtrado.parentElement.classList.add("filtro")
+            console.log(e.target.value)
+        } else {
+            const noFiltrado = producto.parentElement
+            noFiltrado.parentElement.classList.remove("filtro")
+        }
+        
     })
-    console.log(e.target.value)
+    
 })
